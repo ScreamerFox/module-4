d1da04bf810cfa5e48c050cc39225105a9c96250
ScreamerFox <142076193+ScreamerFox@users.noreply.github.com>
2024-07-06 21:34:07 +0500

    Create module_4_1.py

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as dv1
+from true_math import divide as dv2
+
+
+res_1 = dv1(356, 4)
+res_2 = dv1(123, 0)
+res_3 = dv2(12, 4)
+res_4 = dv2(88005353535, 0)
+
+print(res_1)
+print(res_2)
+print(res_3)
+print(res_4)
